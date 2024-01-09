a2ec5df2da4339b57777c26e5a19308ae71f9297
MiThet <mithetthetkhaing.lashio@gmail.com>
2024-01-09 10:07:07 +0630

    fill with data

--- src/main/java/com/example/bookshop/dao/AuthorDao.java
+++ src/main/java/com/example/bookshop/dao/AuthorDao.java
@@ -1,8 +1,8 @@
 package com.example.bookshop.dao;
 
-import com.example.bookshop.entity.Book;
+import com.example.bookshop.entity.Author;
 import com.example.bookshop.entity.BookId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuthorDao extends JpaRepository<Book, BookId> {
+public interface AuthorDao extends JpaRepository<Author, BookId> {
 }
